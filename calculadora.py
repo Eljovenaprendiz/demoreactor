151f6951131d70c4eca55a08525315e3dc9dbbc1
Ignacio Delgado del Valle <apci02@gmail.com>
2023-03-11 19:12:43 +0100

    second commit

--- calculadora.py
+++ calculadora.py
@@ -2,8 +2,15 @@ num1 = 5
 num2 = -2
 
 def suma (numero1, numero2):
-    total = numero1 + numero2
-    return total
+    totalSum = numero1 + numero2
+    return totalSum
 
-resultado = suma(num1, num2)
-print ('El resultado es: ', resultado)
+def resta (numero1, numero2):
+    totalRes = numero1 + numero2
+    return totalRes
+
+resultadoSum = suma(num1, num2)
+
+resultadoRes = resta(num1, num2)
+
+print ('El resultado de la suma es: ', resultadoSum, 'y el resultado de la resta es: ', resultadoRes)
